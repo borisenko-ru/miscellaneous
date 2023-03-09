b27c54a73e13300998fea70664b68d4c38d2b37e
Alexey Borisenko <Borisenko@slb.com>
2023-03-09 13:27:27 +0300

    Minor update

--- 06_wbs/wbs.py
+++ 06_wbs/wbs.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import math
 
 tubing = Image.open('img/tubing.png').convert('RGBA')
 stinger = Image.open('img/stinger.png').convert('RGBA')
@@ -10,17 +11,44 @@ btm_cut = Image.open('img/btm_cut.png').convert('RGBA')
 perf = Image.open('img/perf.png').convert('RGBA')
 
 
-def get_concat_h(im1, im2):
-    dst = Image.new('RGBA', (im1.width + im2.width, im1.height))
-    dst.paste(im1, (0, 0))
-    dst.paste(im2, (im1.width, 0))
+def get_concat_v_multi_resize(im_list, resample=Image.Resampling.BICUBIC):
+    min_width = min(im.width for im in im_list)
+    im_list_resize = [im.resize((min_width, int(im.height * min_width / im.width)),resample=resample)
+                      for im in im_list]
+    total_height = sum(im.height for im in im_list_resize)
+    dst = Image.new('RGBA', (min_width, total_height))
+    pos_y = 0
+    for im in im_list_resize:
+        dst.paste(im, (0, pos_y))
+        pos_y += im.height
     return dst
 
-def get_concat_v(im1, im2):
-    dst = Image.new('RGBA', (im1.width, im1.height + im2.height))
-    dst.paste(im1, (0, 0))
-    dst.paste(im2, (0, im1.height))
-    return dst
+wbs_length = open('wbs.csv', 'r', newline='').read().split(',').count('port')
+split_max = 4
+wbs_split = math.ceil(wbs_length / split_max)
+start = 0
+end = 0
 
-#get_concat_h(im1, im1).save('img/pillow_concat_h.jpg')
-get_concat_v(tubing, get_concat_v(stinger, get_concat_v(packer, get_concat_v(perf, toe)))).save('img/wbs.png')
+while wbs_split > 0:
+    filename = str('img/wbs' + str(wbs_split) + '.png')
+    wbs_iter_list = []
+    wbs_iter = open('wbs.csv', 'r', newline='').read().split(sep=',')
+    for line in wbs_iter:
+        if 'stinger' in line:
+            wbs_iter_list.append(stinger)
+        elif 'packer' in line:
+            wbs_iter_list.append(packer)
+        elif 'port' in line:
+            wbs_iter_list.append(port)
+        elif 'tubing' in line:
+            wbs_iter_list.append(tubing)
+        elif 'toe' in line:
+            wbs_iter_list.append(toe)
+        elif 'perf' in line:
+            wbs_iter_list.append(perf)
+
+    get_concat_v_multi_resize(wbs_iter_list).save(filename)
+    wbs_split -= 1
+#    start = end + 1
+#    end = end + split_max
+    print(wbs_iter_list)
